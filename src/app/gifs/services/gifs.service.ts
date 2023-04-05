fa3e0b520744881a49d7f5c9d5513413251aac29
Hiram <hculebro@neodata.com.mx>
2023-04-05 17:26:24 -0600

    Sidebar working and api call optimization

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGIFResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
@@ -7,16 +7,20 @@ import { Gif, SearchGIFResponse } from '../interfaces/gifs.interface';
 })
 export class GifsService {
 
-  private apiKey: string = 'cRDReQheI53LpbTkncYHM02vIqTnSAeQ';
+  private apiKey: string = 'PUT YOUR OWN API KEY FROM https://developers.giphy.com/';
+  private api: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
   public resultados: Gif[] = []
 
   get historial(): string[] {
-    return [...this._historial]
+    return [...this._historial];
   }
 
-  constructor( private http: HttpClient) {}
+  constructor( private http: HttpClient) {
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+    this.resultados = JSON.parse( localStorage.getItem('resultados')! ) || [];
+  }
 
   buscarGifs( query: string = '' ) {
     query = query.trim().toLowerCase();
@@ -24,12 +28,22 @@ export class GifsService {
     if( !this._historial.includes(query) ) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
 
-    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${ this.apiKey }&q=${ query }&limit=10`)
+    // Esta en usa forma de optimizar los parametros de la api y que no se vea tan horrible ese trozo de codigo
+    const params = new  HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit','10')
+      .set('q',query);
+
+    this.http.get<SearchGIFResponse>(`${ this.api }/search`, {params: params})
       .subscribe( ( response: any ) => {
         console.log( response.data );
         this.resultados = response.data;
+        localStorage.setItem('resultados',JSON.stringify(this.resultados))
       })
   }
 }
